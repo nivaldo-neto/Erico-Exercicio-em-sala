c73bfe2ec437ed9a56283849d36f07044cab6211
nivaldo-neto <57366338+nivaldo-neto@users.noreply.github.com>
2020-02-20 14:15:10 -0300

    Update aprovado.c

--- aprovado.c
+++ aprovado.c
@@ -2,25 +2,34 @@
 
 int main(int argc, char *argv[])
 {
-		int n1, n2, n3, media, SC, F;
+		int n1, n2, n3, media, SC, F, MF;
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 		n3 = atoi(argv[3]);
 		media = (n1 + n2 + n3)/3;
-		if (n1 < 0 || n1 > 10){
+		if (n1 < 0 || n1 > 10) {
 			printf("Digite a nota da segunda chamada: ");
 			scanf("%d", &SC);
 			media = (SC + n2 + n3)/3;
-		}else if(n2 < 0 || n2 > 10){
+		}else if(n2 < 0 || n2 > 10) {
 			printf("Digite a nota da segunda chamada: ");
 			scanf("%d", &SC);
 			media = (n1 + SC + n3)/3;
-		}else if (n3 < 0 || n3> 10){
+		}else if (n3 < 0 || n3> 10) {
 			printf("Digite a nota da segunda chamada: ");
 			scanf("%d", &SC);
 			media = (n1 + n2 + SC)/3;
-		}if (media < 7){
-				printf("Você esta reprovado");		
+		}if (media < 7) {
+			printf("Você esta reprovado\n");		
+			printf("Digite quanto você tirou na final:");
+			scanf("%d", &F);
+			MF = (media + F)/2;
+			if (MF < 5) {
+				printf("Você esta reprovado!");
+			}else {
+				printf("Você esta aprovado!");
+			}
+
 		}else{
 			printf("Você esta aprovado!"); 
 		}
